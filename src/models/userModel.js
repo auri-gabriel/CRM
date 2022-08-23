1f9b1ed5d220a56efd9cd9bc3a7e8dcdf235b87b
Auri Gabriel <ririmello.a@gmail.com>
2022-08-22 23:39:29 -0300

    user model

---
+++ src/models/userModel.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    hashPassword: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
