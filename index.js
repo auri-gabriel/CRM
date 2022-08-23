27783e9c3fa3640e711632c550a7e12330e9d28a
Auri Gabriel <ririmello.a@gmail.com>
2022-08-22 22:33:48 -0300

    static files serving

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ app.use(bodyParser.json());
 
 routes(app);
 
+// serving static files
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.send(`Express running on port ${PORT}`);
 });
