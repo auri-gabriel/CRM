3b0aa2d8fd9a984181602a28aca399c528f9e24a
Auri Gabriel <ririmello.a@gmail.com>
2022-08-23 00:14:55 -0300

    JWT setup

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import routes from './src/routes/crmRoutes';
 import mongoose from 'mongoose';
-import bodyParser from 'body-parser';
+import bodyParser, { json } from 'body-parser';
+import jsonwebtoken from 'jsonwebtoken';
 
 const app = express();
 const PORT = 4000;
@@ -16,6 +17,28 @@ mongoose.connect('mongodb://localhost:27017/CRMdb', {
 // bodyParser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// JWT setup
+app.use((req, res, next) => {
+  if (
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.split(' ')[0] === 'JWT'
+  ) {
+    jsonwebtoken.verify(
+      req.headers.authorization.split(' ')[1],
+      'RESTFULAPIs',
+      (err, decode) => {
+        if (err) req.user = undefined;
+        req.user = decode;
+        next();
+      }
+    );
+  } else {
+    req.user = undefined;
+    next();
+  }
+});
 
 routes(app);
 
